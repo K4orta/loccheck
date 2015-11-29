f701457ee38a5af63cdd6d66da4e5d2170d5e65f
Erik Wong <ewong@blizzard.com>
2015-11-28 17:33:01 -0800

    lint style

--- src/parsing/load-props.js
+++ src/parsing/load-props.js
@@ -1,11 +1,13 @@
 var props = require('properties');
 var fs = require('fs');
 
-module.exports = path => new Promise((resolve, reject) => {
-  fs.readFile(path, (err, data) => {
-    if (err !== null) {
-      reject(err);
-    }
-    resolve(props.parse(String(data)));
+module.exports = path => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err !== null) {
+        reject(err);
+      }
+      resolve(props.parse(String(data)));
+    });
   });
-});
+};
